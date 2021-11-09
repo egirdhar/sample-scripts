1eff5b669a71772e7aaa31dab53c8bcde5eb75d1
girdhar katiyar <girdhar.katiyar@telenetgroup.be>
2021-11-09 23:50:21 +0100

    init commit

---
+++ sample.py
@@ -0,0 +1,9 @@
+class Sample:
+    def __init__(self):
+        super(Sample)
+    
+    def print_message(self, message):
+        print(message)
+
+sample1 = Sample()
+sample1.print_message("Hello World")
