cbc0bf2898628bdbf422414a02a82af8166c0201
girdhar katiyar <girdhar.katiyar@telenetgroup.be>
2021-11-09 23:55:06 +0100

    added perform_action

--- sample.py
+++ sample.py
@@ -1,9 +1,13 @@
 class Sample:
     def __init__(self):
         super(Sample)
+        self._action = None
     
     def print_message(self, message):
         print(message)
+
+    def perform(self, perform_action):
+        self._action = perform_action
 
 sample1 = Sample()
 sample1.print_message("Hello World")
