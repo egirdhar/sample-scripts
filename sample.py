4e4411163ee0df8e6a56d1dfa25928a5263b2d07
egirdhar <egirdhar@gmail.com>
2021-11-09 23:52:25 +0100

    added method
    add action

--- sample.py
+++ sample.py
@@ -4,6 +4,9 @@ class Sample:
     
     def print_message(self, message):
         print(message)
+        
+    def perform(self, action):
+        pass
 
 sample1 = Sample()
 sample1.print_message("Hello World")
